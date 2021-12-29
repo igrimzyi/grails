fac23309e12d47ce4430c49893b9bfd4496c1d7a
igrimzyi <1isaiahofficial1@gmail.com>
2021-12-29 00:38:23 -0800

    Update first-post.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -10,6 +10,7 @@ export default function FirstPost() {
             </Head>
             <h1>First Post</h1>
             <h2>
+
                 <Link href="/">
                 <a>Back to home</a> 
                 </Link>
