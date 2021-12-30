81c82d10c0e2b3e5fe88bc3de24c9fe53f5f209a
igrimzyi <1isaiahofficial1@gmail.com>
2021-12-29 23:22:48 -0800

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,29 @@
+import Head from 'next/head'
+import Image from 'next/image'
 import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import layoutStyles from '../components/layout.module.css'
+import Link from 'next/link'
 
-export default function Layout({ children }) {
-  return <div className={styles.container}>{children}</div>
+const name = 'Your Name'
+export const siteTitle = 'Next.js Sample Website'
+
+export default function Layout({ children, home }) {
+  return (
+   
+      
+          <>
+            <nav className={layoutStyles.navbar}>
+                <Link className="grails" href="/">
+                    <a>Grails</a>
+                </Link>
+                <Link href="/">
+                    <a>Apples</a>
+                </Link>
+
+            </nav>
+          </>
+        
+   )
+        
 }
